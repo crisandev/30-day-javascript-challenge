761c70d899138496026a78826ccc516d5ef20fe8
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2023-11-15 18:50:04 -0400

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+   return (
+      <header>
+         <div className="text-container">
+            <h1>30</h1>
+            <p className="text">Days Vanilla JS Coding Challenge</p>
+         </div>
+      </header>
+   );
+};
+
+export default Main;
